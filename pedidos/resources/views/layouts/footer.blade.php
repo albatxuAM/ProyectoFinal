4384a2926399d82dbedb0cdfdcd6a5dd3ee67ecf
alba <alba.alonso@ikasle.egibide.org>
2023-03-01 10:04:38 +0100

    layout

---
+++ pedidos/resources/views/layouts/footer.blade.php
@@ -0,0 +1,54 @@
+<div class="container">
+<div class="row" tabindex="0" aria-label="Mapa" aria-roledescription="mapa" role="region" style="position: absolute; z-index: 0; left: 0px; top: 0px; height: 100%; width: 100%; padding: 0px; border-width: 0px; margin: 0px; cursor: url('https://maps.gstatic.com/mapfiles/openhand_8_8.cur'), default; touch-action: pan-x pan-y;" aria-describedby="636A639A-06A5-4466-9B4A-2D7513F7038A">
+
+    <div class="col-10" style="z-index: 3; position: absolute; height: 100%; width: 100%; padding: 0px; border-width: 0px; margin: 0px; left: 0px; top: 0px; touch-action: pan-x pan-y;">
+        <div style="z-index: 4; position: absolute; left: 50%; top: 50%; width: 100%; will-change: transform; transform: translate(0px, 0px);">
+            <div style="position: absolute; left: 0px; top: 0px; z-index: 104; width: 100%;"></div>
+            <div style="position: absolute; left: 0px; top: 0px; z-index: 105; width: 100%;"></div>
+            <div style="position: absolute; left: 0px; top: 0px; z-index: 106; width: 100%;"><span id="D8510CC9-AA74-43CF-B15D-6716C2D397B0" style="display: none;">Para desplazarte, pulsa las teclas de flecha.</span>
+                <span id="D8510CC9-AA74-43CF-B15D-6716C2D397B0" style="display: none;">Para desplazarte, pulsa las teclas de flecha.</span>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2925.66784335953!2d-2.6903099845285103!3d42.83762967915769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2ses!4v1677659961420!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+
+
+            <div style="position: absolute; left: 0px; top: 0px; z-index: 107; width: 100%;"></div>
+        </div>
+    </div>
+</div>
+</div>
+<footer class="container_wrap socket_color footer-restaurant " id="socket" role="contentinfo" itemscope="itemscope" itemtype="https://schema.org/WPFooter">
+    <div class="container">
+        <div class="row">
+
+            <div class="col col-md-6">
+                <ul class="listing01">
+                    <li class="head120">ESCUELA DE HOSTELERÍA DE EGIBIDE MENDIZORROTZA</li>
+                    <li>
+                        Amadeo Garcia de Salazar Plaza, 2<br>
+                        01007 Vitoria-Gasteiz Araba </li>
+                </ul>
+            </div>
+
+
+
+
+
+            <div class="col col-md-6">
+                <ul class="social-buttons">
+                    <li class="head120">Síguenos en</li>
+                    <li>
+                        <ul class="social-box">
+                            <li><a target="_blank" class="social-icons facebook" href="https://www.facebook.com/Egibide-Mendizorrotza-Escuela-de-Hosteler%C3%ADa-571303046296664/"></a></li>
+                            <li><a target="_blank" class="social-icons twitter" href="https://twitter.com/egibide?lang=es"></a></li>
+                            <li><a target="_blank" class="social-icons googleplus" href="https://www.instagram.com/egibide_mendizorrotza/"></a></li>
+                            <li><a target="_blank" class="social-icons youtube" href="https://www.youtube.com/user/Egibide"></a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+
+
+</footer>
