c2a394093a2ed2639a0d657875123db478fbbe9a
iker <iker.gomez.rodriguez@ikasle.egibide.org>
2023-03-02 11:47:31 +0100

    Pagina index

--- pedidos/resources/views/layouts/header.blade.php
+++ pedidos/resources/views/layouts/header.blade.php
@@ -1,25 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
     @vite([ 'resources/js/botonesCarritos.js'])
 
     <style>
-        .titulo{
+        .titulo {
             font-size: 3rem;
             font-family: 'Dancing Script', cursive;
             font-weight: bold;
         }
+
         nav {
-            box-shadow: 0 2px 4px 0 rgba(0,0,0,.2);
+            box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .2);
+        }
+
+        #cardIndex {
+            border: none;
         }
     </style>
 </head>
