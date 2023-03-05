1878393f5da5bb3eead7976bf282aa7dd79f418e
celia.garcia1984 <celia.garcia@ikasle.egibide.org>
2023-03-05 17:12:40 +0100

    se carga la cesta vacia y enlace para volver

--- pedidos/resources/views/pages/carrito/show.blade.php
+++ pedidos/resources/views/pages/carrito/show.blade.php
@@ -17,24 +17,32 @@
               </tr>
             </thead>
             <tbody>
-              @foreach(session('carrito') as $key => $value)
+              @if(session()->has('carrito'))
+                @foreach(session('carrito') as $key => $value)
+                <tr>
+                  <td class="td text-center">{{ $value['producto'] }}</td>
+                  <td class="td text-center ">{{ $value['cantidad'] }}</td>
+                  <td class="text-center subtotal" id="{{ $value['id'] }}">{{ $value['precio'] * $value['cantidad'] }}€</td>
+                  <td><a href="#">
+                      <button class="btn btn-outline-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                        </svg></button>
+                    </a></td>
+                  <td><a href="{{ route('carrito.update', $value['id']) }}">
+                      <span class="material-symbols-outlined">add</span>
+                    </a>
+                  </td>
+                </tr>
+                @endforeach
+              
+              @else
               <tr>
-                <td class="td text-center">{{ $value['producto'] }}</td>
-                <td class="td text-center ">{{ $value['cantidad'] }}</td>
-                <td class="text-center subtotal" id="{{ $value['id'] }}">{{ $value['precio'] * $value['cantidad'] }}€</td>
-                <td><a href="#">
-                    <button class="btn btn-outline-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-                      </svg></button>
-                  </a></td>
-                <td><a href="{{ route('carrito.update', $value['id']) }}">
-                    <span class="material-symbols-outlined">add</span>
-                  </a>
-                </td>
-              </tr>
-              @endforeach
-
+                  <td class="td text-center" colspan="4">La cesta esta vacía.</td>
+                   <td><a href="{{ route('productos.index') }}">Seguir mirando</a></td>
+                  
+                </tr>
+              @endif
             </tbody>
           </table>
         </div>
