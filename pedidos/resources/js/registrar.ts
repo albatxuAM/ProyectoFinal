6eba93df15a4193e7538b839589988f3abea8ae2
Alba <alba.alonso@ikasle.egibide.org>
2023-03-09 12:06:14 +0100

    recuperar cambios

--- pedidos/resources/js/registrar.ts
+++ pedidos/resources/js/registrar.ts
@@ -1,19 +1,24 @@
-const registrar = document.getElementById("registrar") as HTMLButtonElement;
-const vista = document.getElementById("vista");
-registrar?.addEventListener('click',verregistro);
+const registrar:HTMLElement =<HTMLElement> document.getElementById("registrar");
+const vista:HTMLDivElement = <HTMLDivElement>document.getElementById("vista");
+registrar?.addEventListener('click',verRegistro);
+var cardPrincipal:HTMLElement =<HTMLElement> document.getElementById('cardPrincipal');
 
 
-function verregistro():void {
+
+function verRegistro():void {
     var x = registrar;
     if (x.innerHTML=='atras') {
       vista?.setAttribute("style", "display:none;");
         x.innerHTML='nuevo usuario';
     } else {
         vista?.setAttribute("style", "display:block;");
+       // cardPrincipal.setAttribute('style','display:none;');
         x.innerHTML='atras';
     }
     vista?.classList.toggle('d-flex');
-    
 }
+
+
+
 
 
